58ebd3b470a83e4f8db81c52f4ba01c85d214a4b
Luciana Quiroga <8123456+lquiroga-dev@users.noreply.github.com>
2021-07-15 22:41:09 -0300

    fix #5: UTM en hemisferio sur suma el falso norte

--- noa_geo/utm.py
+++ noa_geo/utm.py
@@ -8,6 +8,8 @@ import math
 
 from .gauss_krueger import A, E2, K0, _arco_meridiano
 
+FALSO_NORTE_SUR = 10000000.0
+
 
 def zona_utm(lon):
     """Zona UTM (1-60) para una longitud en grados."""
@@ -19,7 +21,10 @@ def meridiano_zona(zona):
 
 
 def latlon_a_utm(lat, lon, zona=None):
-    """lat/lon -> (este, norte, zona). Hemisferio sur TODO: ver issue #5."""
+    """lat/lon -> (este, norte, zona).
+
+    En hemisferio sur se suma el falso norte de 10.000.000 m (fix #5).
+    """
     if zona is None:
         zona = zona_utm(lon)
     phi = math.radians(lat)
@@ -33,8 +38,19 @@ def latlon_a_utm(lat, lon, zona=None):
     m = _arco_meridiano(phi)
 
     este = 500000.0 + K0 * nu * (
-        lam * cosp + lam**3 * cosp**3 / 6 * (1 - t * t + ep2)
+        lam * cosp
+        + lam**3 * cosp**3 / 6 * (1 - t * t + ep2)
+        + lam**5 * cosp**5 / 120 * (5 - 18 * t * t + t**4 + 14 * ep2 - 58 * t * t * ep2)
     )
-    norte = K0 * (m + nu * t * lam**2 * cosp**2 / 2)
-    # TODO(issue #5): en hemisferio sur hay que sumar 10.000.000 al norte
+    norte = K0 * (
+        m
+        + nu * t
+        * (
+            lam**2 * cosp**2 / 2
+            + lam**4 * cosp**4 / 24 * (5 - t * t + 9 * ep2 + 4 * ep2 * ep2)
+            + lam**6 * cosp**6 / 720 * (61 - 58 * t * t + t**4)
+        )
+    )
+    if lat < 0:  # hemisferio sur — fix #5
+        norte += FALSO_NORTE_SUR
     return este, norte, zona
--- tests/test_utm.py
+++ tests/test_utm.py
@@ -1,3 +1,5 @@
+import math
+
 import pytest
 
 from noa_geo.utm import latlon_a_utm, zona_utm
@@ -7,7 +9,16 @@ def test_zona_utm_salta():
     assert zona_utm(-65.4) == 20
 
 
-def test_utm_basico():
-    e, n, z = latlon_a_utm(-24.78, -65.42)
-    assert z == 20
-    assert 500_000 < e < 900_000
+def test_hemisferio_sur_suma_falso_norte():
+    _, n_sur, _ = latlon_a_utm(-24.78, -65.42)
+    _, n_norte, _ = latlon_a_utm(24.78, -65.42)
+    assert n_sur > 7_000_000
+    assert n_norte < 4_000_000
+
+
+def test_localidades_cargan():
+    from noa_geo.localidades import buscar
+
+    res = buscar("cafayate")
+    assert len(res) == 1
+    assert res[0].provincia == "Salta"
